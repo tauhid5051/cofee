d8eae18d035a53151328afed9a91b19b2ba0ad47
tauhid5051 <tauhid5051@gmail.com>
2026-07-26 17:19:25 +0600

    last stable 26/7/26

--- vendor/arutil/ar-php/src/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-defined('ARUTIL_ROOT') ?: define('ARUTIL_ROOT', dirname(__DIR__));
-
--- vendor/tecdiary/sms/src/Gateways/SmsGatewayInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tecdiary\Sms\Gateways;
-
-interface SmsGatewayInterface
-{
-    public function getUrl();
-    public function sendSms($mobile, $message);
-}
